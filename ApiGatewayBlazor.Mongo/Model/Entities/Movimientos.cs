095cca2afcb303a718d2a8a4d16c88acca45ba56
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2023-08-22 03:45:57 -0700

    Se agrego el CRUD para SQL y se eliminaron carpetas que ya no se ocupan

---
+++ ApiGatewayBlazor.Mongo/Model/Entities/Movimientos.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Diagnostics.Contracts;
+
+
+namespace ApiGatewayBlazor.Mongo.Model.Entities
+
+
+{
+    public class Movimientos
+    {
+
+        ////Conectar a la base de datos
+        //MongoClient client = new MongoClient("mongodb://localhost:27017");
+        //IMongoDatabase database = client.GetDatabase("agenda");
+        ////Coleccion = tabla
+        //IMongoCollection<Contacto> collection = database.GetCollection<Contacto>("contactos");
+
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+        public string ProductoId { get; set; } = string.Empty;
+        public string ProductoDescripcion { get; set; } = string.Empty;
+
+        //[BsonRepresentation("like")]
+        //public string Like { get; set; } = string.Empty;
+        //[BsonRepresentation("dislike")]
+        public string Dislike { get; set; } = string.Empty;
+        public string Venta { get; set; } = string.Empty;
+
+        public string ClienteId { get; set; } = string.Empty;
+
+        public string ClienteNombre { get; set; } = string.Empty;
+
+}
+}
--- ApiGatewayBlazor.SqlServer/Model/Entities/Cliente.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApiGatewayBlazor.SqlServer.Model.Entities
-{
-    public class Cliente
-    {
-        public int Id { get; set; } 
-        public string Nombre { get; set; }
-    }
-}
