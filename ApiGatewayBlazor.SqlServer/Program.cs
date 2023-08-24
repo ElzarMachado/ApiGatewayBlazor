3aef61ba9f1725022501b7f744b6080196c25a40
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2023-08-23 18:51:35 -0700

    Intentando con codigo diferente para resolver error de swagger pero no sirvio

--- ApiGatewayBlazor.SqlServer/Program.cs
+++ ApiGatewayBlazor.SqlServer/Program.cs
@@ -1,22 +1,59 @@
+//using ApiGatewayBlazor.SqlServer.Datos;
+//using ApiGatewayBlazor.SqlServer.Models;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//        options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
 using ApiGatewayBlazor.SqlServer.Datos;
 using ApiGatewayBlazor.SqlServer.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
-
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("SqlConnection")));
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
 
 var app = builder.Build();
 
